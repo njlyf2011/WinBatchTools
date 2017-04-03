87acc0e1d30848f69b5d0907f812c2bfd2d48218
njlyf2011 <njlyf2011@hotmail.com>
2017-04-03 18:03:26 +0800

    任何的修改也要按照 GitHub 的基本法，按照选举法去产生！
    没啥。。。做了点修改

--- system/screenfetch.bat
+++ system/screenfetch.bat
@@ -1,6 +1,5 @@
 @echo off
-
-rem Windows Batch Tools Copyright (C) 2016 NJLYF2011, Leo3418
+rem Windows Batch Tools Copyright (C) 2016-2017 NJLYF2011, Leo3418
 rem 本程序从未提供品质担保。这是款自由软件，欢迎你在满足一定条件后对其再发布。
 
 setlocal ENABLEDELAYEDEXPANSION
