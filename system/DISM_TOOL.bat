43e4cd4b15ff32e222d2a362e3ed8c17b473e924
lini3acc <myzhanghao@live.com>
2017-08-15 20:36:33 +0800

    Feature Update

--- system/DISM_TOOL.bat
+++ system/DISM_TOOL.bat
@@ -6,6 +6,8 @@ cls
 echo.
 echo DISM简易辅助工具(Beta)
 echo.
+echo 注意：本程序需要以管理员身份运行！
+echo.
 echo 请选择操作：
 echo.
 echo (1)获取镜像信息
@@ -13,6 +15,7 @@ echo (2)挂载镜像
 echo (3)展开镜像
 echo (4)卸载镜像
 echo (5)将目录保存到镜像
+echo (6)退出本程序
 echo.
 :ch
 set /p cho= 请选择：
@@ -21,19 +24,44 @@ if %cho% EQU 2 goto ch2
 if %cho% EQU 3 goto ch3
 if %cho% EQU 4 goto ch4
 if %cho% EQU 5 goto ch5
+if %cho% EQU 6 goto end
 goto ch
 :ch1
-echo 此功能将在未来加入，敬请期待！
+set /p imgfile= 请输入镜像文件的位置（如果目录有空格，请打双引号）：
+dism /Get-Imageinfo /imagefile:%imgfile%
 goto ch
 :ch2
-echo 此功能将在未来加入，敬请期待！
+set /p imgfile= 请输入镜像文件的位置（如果目录有空格，请打双引号）：
+set /p mntdir= 请输入镜像挂载目录（如果目录有空格，请打双引号）：
+set /p idx= 请输入镜像索引号（如没有则输入0）：
+set /p rdo= 是否只读(Y/N)：
+if "%rdo%"=="Y" (
+    if "%idx%"=="0" (
+        dism /Mount-Image /ImageFile:%imgfile% /MountDir:%mntdir% /ReadOnly
+        goto ch
+    )
+    dism /Mount-Image /ImageFile:%imgfile% /Index:%idx% /MountDir:%mntdir% /ReadOnly
+    goto ch
+)
+if "%idx%"=="0" (
+    dism /Mount-Image /ImageFile:%imgfile% /MountDir:%mntdir% 
+    goto ch
+)
+dism /Mount-Image /ImageFile:%imgfile% /Index:%idx% /MountDir:%mntdir%
 goto ch
 :ch3
 echo 此功能将在未来加入，敬请期待！
 goto ch
 :ch4
-echo 此功能将在未来加入，敬请期待！
+set /p mntdir= 请输入镜像挂载目录（如果目录有空格，请打双引号）：
+set /p comorn= 是否保存镜像(Y/N)：
+if "%comorn%"=="Y" (
+    dism /Unmount-Image /MountDir:%mntdir% /Commit
+    goto ch
+)
+dism /Unmount-Image /MountDir:%mntdir%
 goto ch
 :ch5
 echo 此功能将在未来加入，敬请期待！
 goto ch
+:end
